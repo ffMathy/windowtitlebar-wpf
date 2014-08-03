948421a5fe2377e1577cb8da21878946b86671d5
ffMathy <mathias.lorenzen@live.com>
2014-08-03 15:55:46 +0200

    basic structure.

---
+++ WindowTitleControl/WindowTitleControl/WindowTitleControl.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace Controls
+{
+    /// <summary>
+    /// Interaction logic for WindowTitleControl.xaml
+    /// </summary>
+    public partial class WindowTitleControl : UserControl
+    {
+        private Window Window
+        {
+            get { return Parent as Window; }
+        }
+
+        public WindowTitleControl()
+        {
+            InitializeComponent();
+        }
+
+        private void DraggableBar(object sender, MouseButtonEventArgs e)
+        {
+            Window.DragMove();
+        }
+    }
+}
